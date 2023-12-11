36d4c51740176a16bf9916029b7d15034550de9f
Kawal Deol <35369346+KawalpreetDeol@users.noreply.github.com>
2023-12-11 07:59:26 -0500

    word cloud changes

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,6 +1,11 @@
 // Skills.js
 import React from 'react';
 import WordCloud from 'react-wordcloud';
+import "d3-transition";
+import { select } from "d3-selection";
+
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
 
 const Skills = ({ skills }) => {
   const wordcloudOptions = {
@@ -8,13 +13,26 @@ const Skills = ({ skills }) => {
     fontSizes: [20, 60],
     fontWeight: 'bold',
     rotations: 1,
-    rotationAngles: [0],
+    rotationAngles: [0]
   };
+
+  const getCallback = (callback) => {
+    return function (word, event) {
+      const isActive = callback !== "onWordMouseOut";
+      const element = event.target;
+      const text = select(element);
+      word.fontSize = word.fontSize ? word.fontSize : `${word.size*.95}px`;
+      text
+        .transition()
+        .attr("background", "white")
+        .attr("font-size", isActive ? "200%" : word.fontSize);
+    };
+  }
   const callbacks = {
     getwordColor: (word) => (word.value > 50 ? "orange" : "purple"),
     getWordTooltip: (word) => `${word.fullForm}`,
-    // onWordMouseOver: (word) =>``,
-    // onWordClick: (word) =>``
+    onWordMouseOver: getCallback("onWordMouseOver"),
+    onWordMouseOut: getCallback("onWordMouseOut")
   }
 
   return <WordCloud words={skills} callbacks={callbacks} options={wordcloudOptions} style={{maxWidth: '100vw', maxHeight: '100vh'}}/>;
