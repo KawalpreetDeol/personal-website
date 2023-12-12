8eac17b6fbbc85cc830490d5a45696c4ce2a6421
Kawal Deol <35369346+KawalpreetDeol@users.noreply.github.com>
2023-12-12 09:04:15 -0500

    contact me page

--- src/views/ContactMe.js
+++ src/views/ContactMe.js
@@ -1,13 +1,116 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  TextField,
+  Button,
+  Typography,
+  Snackbar,
+  Container,
+  Paper,
+} from '@material-ui/core';
 
-function ContactMe() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  formContainer: {
+    padding: theme.spacing(4),
+    boxShadow: '0px 3px 15px rgba(0, 0, 0, 0.2)',
+    borderRadius: theme.spacing(1),
+    width: '100%',
+    maxWidth: '400px',
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
+  submitButton: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const ContactMe = () => {
+  const classes = useStyles();
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isSnackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+    // For this example, we'll just display a snackbar
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
-    <Box>
-        <h1>Contact Me</h1>
-    </Box>
+    <Container className={classes.root} style={{minHeight: '84vh', maxHeight: '100vh'}}>
+      <Paper className={classes.formContainer}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Contact Me
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            className={classes.textField}
+            label="Name"
+            name="name"
+            variant="outlined"
+            fullWidth
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            className={classes.textField}
+            label="Email"
+            name="email"
+            type="email"
+            variant="outlined"
+            fullWidth
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            className={classes.textField}
+            label="Message"
+            name="message"
+            multiline
+            rows={4}
+            variant="outlined"
+            fullWidth
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+          <Button
+            className={classes.submitButton}
+            variant="contained"
+            color="primary"
+            type="submit"
+            fullWidth
+          >
+            Submit
+          </Button>
+        </form>
+      </Paper>
+      <Snackbar
+        open={isSnackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message="Form submitted successfully!"
+      />
+    </Container>
   );
-}
+};
 
 export default ContactMe;
